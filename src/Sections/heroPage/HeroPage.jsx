bc506f2fdf692c2614090a89848f070e594feeb5
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2023-02-16 16:01:26 +0600

    change the about me

--- src/Sections/heroPage/HeroPage.jsx
+++ src/Sections/heroPage/HeroPage.jsx
@@ -7,7 +7,7 @@ function HeroPage() {
         <Grid container spacing={4} alignItems="center" style={{minHeight:"100vh"}} >
             <Grid item xs={12} md={6} >
                 <h3 className = "name">Rakib Hasan Chowdhury</h3>
-                <p className='about'>Dedicated professional with over 8 years of active marketing experience. Looking to bring my marketing experience in developing and executing marketing strategies with the unique ability to understand the ongoing market scenario and customers trends using exceptional marketing communication skills.</p>
+                <p className='about'>Dedicated professional with over a decade of active marketing experience. Looking to bring my marketing experience in developing and executing marketing strategies with the unique ability to understand the ongoing market scenario and customers trends using exceptional marketing communication skills.</p>
                 <br/>
                 <Contacts/>
                 <br />
